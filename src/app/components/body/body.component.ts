6fb4d42ec633c572e22f01fcfae8c15cedec884a
Carolina <carovel2608@gmail.com>
2025-10-07 18:40:37 -0500

    delete side panel

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -12,12 +12,41 @@ export class BodyComponent {
   selectedLogo: { id: number; name: string; src: string } | null = null;
 
   logos = [
-    { id: 1, name: 'Sicope', src: '../../../assets/logos/sicope.png' },
-    { id: 2, name: 'Sinope', src: '../../../assets/logos/sinope.png' },
-    { id: 3, name: 'Coper', src: '../../../assets/logos/coper.png' },
-    { id: 4, name: 'Fovid', src: '../../../assets/logos/fovid.png' },
-    { id: 5, name: 'Astra', src: '../../../assets/logos/astra.png' }
+    {
+      id: 1,
+      name: 'Sicope',
+      src: '../../../assets/logos/sicope.png',
+      url: 'https://sicope.com',
+    },
+    {
+      id: 2,
+      name: 'Sinope',
+      src: '../../../assets/logos/sinope.png',
+      url: 'https://sinope.com',
+    },
+    {
+      id: 3,
+      name: 'Coper',
+      src: '../../../assets/logos/coper.png',
+      url: 'https://caocc.ejercito.mil.co/SiwepDiper-web/faces/index.xhtml',
+    },
+    {
+      id: 4,
+      name: 'Fovid',
+      src: '../../../assets/logos/fovid.png',
+      url: 'https://fovid.ejercito.mil.co/Fovid-2.0/faces/index.xhtml',
+    },
+    {
+      id: 5,
+      name: 'Astra',
+      src: '../../../assets/logos/astra.png',
+      url: 'https://astra.com',
+    },
   ];
+
+  onClickLogo(logo: { id: number; name: string; src: string; url: string }) {
+    window.open(logo.url, '_blank');
+  }
 
   selectLogo(logo: { id: number; name: string; src: string }) {
     this.selectedLogo = logo;
